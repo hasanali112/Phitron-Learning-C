5198781b50b0c18fc162d1622bd3c10ec6e62093
hasanali112 <sohagali.ru.ac@gmail.com>
2025-02-23 10:41:35 +0600

    conditional statement

---
+++ module 2/conditional.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+    
+    int tk;
+    scanf("%d", &tk);
+
+    if (tk>=100)
+    {
+      printf("burgur ami khabo");
+    }
+    else if (tk>= 50)
+    {
+        printf("Fuchka khabo");
+    }
+    else{
+        printf("kisui khabo na");
+    }
+    
+    
+
+    return 0;
+}
