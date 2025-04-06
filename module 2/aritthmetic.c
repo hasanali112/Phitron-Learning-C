18001140c70a8e61183044f5b7fddcdfb7bf2389
hasanali112 <sohagali.ru.ac@gmail.com>
2025-04-06 23:22:06 +0600

    finish arithmetic operators

--- module 2/aritthmetic.c
+++ module 2/aritthmetic.c
@@ -1,9 +1,17 @@
-#include<stdio.h>
-int main(){
+#include <stdio.h>
+int main()
+{
 
-    int a = 5 , b=2;
-    float dvd = a*1.0/b;
-    printf("%f", dvd);
+    float a = 8;
+    int b = 5;
+    // int sum = a + b;
+    // int sub = a - b;
+    // int mul = a * b;
+    // printf("sum = %d\n", sum);
+    // printf("sub = %d\n", sub);
+    // printf("mul = %d\n", mul);
+    float dvd = a / b;
+    printf("dvd = %f", dvd);
 
     return 0;
 }
