b316ed68be1a06106d11ad82deba808462f91f2a
hasanali112 <sohagali.ru.ac@gmail.com>
2025-04-08 21:42:28 +0600

    arthimatic operator

--- module 2/aritthmetic.c
+++ module 2/aritthmetic.c
@@ -4,14 +4,14 @@ int main()
 
     float a = 8;
     int b = 5;
-    // int sum = a + b;
-    // int sub = a - b;
-    // int mul = a * b;
-    // printf("sum = %d\n", sum);
-    // printf("sub = %d\n", sub);
-    // printf("mul = %d\n", mul);
+    int sum = a + b;
+    int sub = a - b;
+    int mul = a * b;
     float dvd = a / b;
-    printf("dvd = %f", dvd);
+    printf("sum = %d\n", sum);
+    printf("sub = %d\n", sub);
+    printf("mul = %d\n", mul);
+    printf("dvd = %.1f", dvd);
 
     return 0;
 }
---
+++ module 2/mode.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int a = 18;
+    int b = 5;
+    int rem = 12 % 5;
+    printf("%d", rem);
+    return 0;
+}
