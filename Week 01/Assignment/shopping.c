b62220d9778896f90b092eea5d9d0572efadfd0a
hasanali112 <sohagali.ru.ac@gmail.com>
2025-04-11 16:49:07 +0600

    complete week one

---
+++ Week 01/Assignment/shopping.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    long long int N;
+    scanf("%lld", &N);
+    if (N > 1000)
+    {
+        printf("I will buy Punjabi\n");
+        if (N - 1000 >= 500)
+        {
+            printf("I will buy new shoes\nAlisa will buy new shoes");
+        }
+    }
+    else
+    {
+        printf("Bad luck!");
+    }
+
+    return 0;
+}
---
+++ Week 01/Assignment/tempCodeRunnerFile.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+    long long multi = a * b;
+    printf("%lld", multi);
+    return 0;
+}
