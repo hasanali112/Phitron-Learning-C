e83116cb474f788ff6f44eedc72b28ff53fbb617
hasanali112 <sohagali.ru.ac@gmail.com>
2025-04-19 21:18:49 +0600

    finish 2 practice problem

---
+++ Week 02/practice 2/C_Replacement.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+{
+    int N;
+    scanf("%d", &N);
+    int arr[N];
+    for (int i = 0; i < N; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    for (int i = 0; i < N; i++)
+    {
+        if (arr[i] > 0)
+        {
+            arr[i] = 1;
+        }
+        else if (arr[i] < 0)
+        {
+            arr[i] = 2;
+        }
+        else
+        {
+            arr[i] = 0;
+        }
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
