352ed560f06b515fd056c1aca66f5bdb542f589d
hasanali112 <sohagali.ru.ac@gmail.com>
2025-05-05 22:47:05 +0600

    add week 03

--- Week 03/assingment/Count_Me_4.c
+++ Week 03/assingment/Count_Me_4.c
@@ -1,8 +1,25 @@
+
 #include <stdio.h>
+#include <string.h>
 int main()
 {
     char s[100001];
     scanf("%s", s);
+    int str[26] = {0};
+    for (int i = 0; i < strlen(s); i++)
+    {
+        int val = s[i] - 97;
+        str[val]++;
+    }
+
+    for (int i = 0; i < 26; i++)
+    {
+
+        if (str[i] > 0)
+        {
+            printf("%c - %d\n", i + 97, str[i]);
+        }
+    }
 
     return 0;
 }
---
+++ Week 04/sum_of_values_equal.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int a[n];
+    int sum = 0;
+    scanf("%d", &sum);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    for (int i = 0; i < n; i++)
+    {
+        }
+    return 0;
+}
